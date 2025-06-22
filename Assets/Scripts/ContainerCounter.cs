f38f96905f06b602135946ac57d4f1c8afa17ac6
Kushaal Challani <66863823+kushaalchallani@users.noreply.github.com>
2025-06-22 11:44:11 +0530

    [Modified] Refactored to clean code

--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -1,39 +1,14 @@
 using UnityEngine;
 
-public class ContainerCounter : BaseCounter, IKitchenObjectParent {
+public class ContainerCounter : BaseCounter {
 
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
-    [SerializeField] private Transform counterTopPoint;
 
-    private KitchenObject kitchenObject;
     public override void Interact(Player player) {
-        if (kitchenObject == null) {
-            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
-        } else {
-            // Give object to player
-            kitchenObject.SetKitchenObjectParent(player);
+        if (!HasKitchenObject()) {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
         }
-
     }
 
-    public Transform GetKitchenObjectFollowTransform() {
-        return counterTopPoint;
-    }
-
-    public void SetKitchenObject(KitchenObject kitchenObject) {
-        this.kitchenObject = kitchenObject;
-    }
-
-    public KitchenObject GetKitchenObject() {
-        return kitchenObject;
-    }
-
-    public void clearKitchenObject() {
-        kitchenObject = null;
-    }
-
-    public bool HasKitchenObject() {
-        return kitchenObject != null;
-    }
 }
