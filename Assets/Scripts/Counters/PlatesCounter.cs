ce631bad95460db26ffbdfbff94e9ea9ff8ea4a2
Kushaal Challani <66863823+kushaalchallani@users.noreply.github.com>
2025-06-25 13:14:41 +0530

    [Added] Plates Counter

---
+++ Assets/Scripts/Counters/PlatesCounter.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlatesCounter : BaseCounter {
+    public event EventHandler OnPlateSpawned;
+    public event EventHandler OnPlateRemoved;
+
+    [SerializeField] private KitchenObjectSO plateKitchenObjectSO;
+    private float spawnPlateTimer;
+    private float spawnPlateTimerMax = 4f;
+    private int platesSpawnedAmount;
+    private int platesSpawnedAmountMax = 4;
+
+    private void Update() {
+        spawnPlateTimer += Time.deltaTime;
+        if (spawnPlateTimer > spawnPlateTimerMax) {
+            spawnPlateTimer = 0f;
+
+            if (platesSpawnedAmount < platesSpawnedAmountMax) {
+                platesSpawnedAmount++;
+
+                OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public override void Interact(Player player) {
+        if (!player.HasKitchenObject()) {
+            //player empty hands
+            if (platesSpawnedAmount > 0) {
+                //has atleast one plate
+                platesSpawnedAmount--;
+
+                KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, player);
+                OnPlateRemoved?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
