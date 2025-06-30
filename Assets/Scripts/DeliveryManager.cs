a9b6369f1136acb38d11a8749ea1384f30aced74
Kushaal Challani <66863823+kushaalchallani@users.noreply.github.com>
2025-06-30 15:58:10 +0530

    [Added] Sound effects on delivery success & failed

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -6,6 +6,8 @@ public class DeliveryManager : MonoBehaviour {
 
     public event EventHandler OnRecipeSpawned;
     public event EventHandler OnRecipeCompleted;
+    public event EventHandler OnRecipeSuccess;
+    public event EventHandler OnRecipeFailed;
 
     public static DeliveryManager Instance { get; private set; }
     [SerializeField] private RecipeListSO recipeListSO;
@@ -59,12 +61,14 @@ public class DeliveryManager : MonoBehaviour {
                     // player deliver the correct recipe!
                     waitingRecipeSOList.RemoveAt(i);
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
+                    OnRecipeSuccess?.Invoke(this, EventArgs.Empty);
                     return;
                 }
             }
         }
         //no match found!
         //player did not deliver a correct recipe
+        OnRecipeFailed?.Invoke(this, EventArgs.Empty);
 
     }
 
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+    [SerializeField] private AudioClipsRefsSO audioClipsRefsSO;
+
+    public void Start() {
+        DeliveryManager.Instance.OnRecipeSuccess += DeliveryManager_OnRecipeSuccess;
+        DeliveryManager.Instance.OnRecipeFailed += DeliveryManager_OnRecipeFailed;
+    }
+
+    private void DeliveryManager_OnRecipeSuccess(object sender, System.EventArgs e) {
+        DeliveryCounter deliveryCounter = DeliveryCounter.Instance;
+        PlaySound(audioClipsRefsSO.deliverySuccess, deliveryCounter.transform.position);
+    }
+    private void DeliveryManager_OnRecipeFailed(object sender, System.EventArgs e) {
+        DeliveryCounter deliveryCounter = DeliveryCounter.Instance;
+        PlaySound(audioClipsRefsSO.deliveryFail, deliveryCounter.transform.position);
+    }
+    private void PlaySound(AudioClip[] audioClipArray, Vector3 position, float volume = 1f) {
+        PlaySound(audioClipArray[Random.Range(0, audioClipArray.Length)], position, volume);
+    }
+    private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f) {
+        AudioSource.PlayClipAtPoint(audioClip, position, volume);
+    }
+}
